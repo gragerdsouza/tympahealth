dd8f8ca72aea5f0527622c0cee21d462b0d74265
gragerdsouza <gragerdsouza@gmail.com>
2022-08-14 23:59:58 +0100

    Changes in API Route

--- routes/api.php
+++ routes/api.php
@@ -20,17 +20,3 @@ Route::resource('devices', DeviceController::class)
         ->missing(function (Request $request) {
             return Redirect::route('devices.index');
         });
-Route::get('search',[DeviceController::class, 'search']);
-/*Route::resource('devices', DeviceController::class)
-->except([
-    'create', 'store', 'update', 'destroy'
-]);
-
-Route::resource('devices', DeviceController::class)->only([
-    'index', 'show'
-]);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
